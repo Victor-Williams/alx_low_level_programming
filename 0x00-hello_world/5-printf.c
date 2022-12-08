a7c10c94c979a182e337cac4f3eca3d37636cfe0
victorwilliams <victorelegbede@gmail.com>
2022-12-08 09:17:10 -0800

    Printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+* main - prints phrase
+*
+* Return: 0 if success otherwise negative
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of,\n");
+	return (0);
+}
