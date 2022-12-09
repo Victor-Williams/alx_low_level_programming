3ee1fbb93d8cdf050572740148db0dd7255de14f
victorwilliams <victorelegbede@gmail.com>
2022-12-09 08:33:20 -0800

    Print alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
-* main - prints the alphabet in lowercase
+* main - prints the alphabet in lowercase,
 * followed by new line
 * Return: Always (0) success
 */
@@ -13,6 +13,6 @@ int main(void)
 	{
 		putchar(ch);
 	}
-	putchar("/n");
+	putchar('/n');
 	return (0);
 }
